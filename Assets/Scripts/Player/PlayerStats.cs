63feadd043d785a278e1af5017eda609b5640966
Nahuel Aparicio <nahuel.aparicio@enti.cat>
2024-10-17 18:57:13 +0200

    BaseProject

---
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    private float _speed;
+    public float Speed { get { return _speed; } }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
